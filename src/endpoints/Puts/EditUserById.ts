cd7130df6a7240f341a39e9882ff99836c2ffee4
Samuel Pereira <108569272+eu-samuel@users.noreply.github.com>
2023-07-26 16:11:56 -0300

    Resolvendo bugs.

--- src/endpoints/Puts/EditUserById.ts
+++ src/endpoints/Puts/EditUserById.ts
@@ -8,7 +8,6 @@ export const editUserById = async (req: Request, res: Response) => {
     const newName = req.body.name as string | undefined
     const newEmail = req.body.email as number | undefined
     const newPassword = req.body.password as string | undefined
-    const lastUpdate = new Date().toISOString()
 
     try {
         const [user] = await db.select("*").from("users").where({ id: id })
@@ -18,24 +17,19 @@ export const editUserById = async (req: Request, res: Response) => {
             name: newName || user.name,
             email: newEmail || user.email,
             password: newPassword || user.password,
-            lastUpdate: lastUpdate
         }).from("users").where({ id: id })
 
         if (!user) {
-             res.status(400)
+            res.status(400)
             throw new Error("Usuário não encontrado, id inválido.")
         }
-
-        if (typeof newName !== "string" || typeof newEmail !== "string" ||
-            typeof newPassword !== "string") {
-             res.status(400)
-            throw new Error("Por favor, preencha com strings os campos que pedem esse formato.")
-        }
-
 
         res.status(200).send({ message: "Usuário atualizado com sucesso!" })
 
     } catch (error: any) {
-        res.status(500).send("Erro desconhecido, faça uma nova requisição.")
+        if (error instanceof Error) {
+            res.status(400).send("Não foi possível realizar as alterações, faça uma nova requisição.")
+        }
+        res.status(500).send("Erro desconhecido.")
     }
 }
