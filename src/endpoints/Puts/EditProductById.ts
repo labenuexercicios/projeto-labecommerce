8e4330f80e6f00b4ab3acacba1675404d970e25d
Samuel Pereira <108569272+eu-samuel@users.noreply.github.com>
2023-07-25 23:00:02 -0300

    Resolvendo bugs

--- src/endpoints/Puts/EditProductById.ts
+++ src/endpoints/Puts/EditProductById.ts
@@ -5,41 +5,34 @@ export const editProductById = async (req: Request, res: Response) => {
 
     const id = req.params.id
 
-    const newId = req.body.id as string | undefined
     const newName = req.body.name as string | undefined
     const newPrice = req.body.price as number | undefined
     const newDescription = req.body.description as string | undefined
     const newImage_url = req.body.image_url as string | undefined
-    const lastUpdate = new Date().toISOString()
 
     try {
-        const [product] = await db.select("*").from("products").where({ id: id })
+        const [product] = await db.select().from("products").where({ id: id })
 
-        await db.update({
-            id: newId || product.id,
+        if (!product) {
+            res.status(400)
+            throw new Error("Insira um Id válido.")
+        }
+
+        if(product) {
+            await db.update({
+            id: product.id,
             name: newName || product.name,
             price: newPrice || product.price,
             description: newDescription || product.description,
             image_url: newImage_url || product.image_url,
-            lastUpdate: lastUpdate
         }).from("products").where({ id: id })
-
-        if (typeof id !== "string") {
-             res.status(400)
-            throw new Error("Por favor, o id deve ser uma string.")
-        }
-
-        if (typeof newId !== "string" || typeof newName !== "string" ||
-            typeof newDescription !== "string" || typeof newImage_url !== "string") {
-             res.status(400)
-            throw new Error("Por favor, preencha com strings os campos que pedem esse formato.")
         }
 
         res.status(200).send({ message: "Produto atualizado com sucesso!" })
 
     } catch (error: any) {
         if (error instanceof Error) {
-            res.status(400).send("Insira um Id válido.")
+            res.status(400).send("Não foi possível realizar as alterações, produto não encontrado.")
         }
         res.status(500).send("Erro desconhecido, faça uma nova requisição.")
     }
